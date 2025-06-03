4b2964aa93eccb8d76602cf87c5e79d45362aac2
Your Name <you@example.com>
2025-06-03 21:56:35 +0530

    changed some dialouge

--- src/customers/dialogue.rs
+++ src/customers/dialogue.rs
@@ -19,6 +19,6 @@ impl Plugin for DialogPlugin {
 pub fn spawn_dialogue_runner(mut commands: Commands, project: Res<YarnProject>) {
     println!("Spawning dialogue runner with project: {:?}", project);
     let mut dialogue_runner = project.create_dialogue_runner(&mut commands);
-    dialogue_runner.start_node("HelloWorld");
+    dialogue_runner.start_node("BartenderMonologue");
     commands.spawn(dialogue_runner);
 }
