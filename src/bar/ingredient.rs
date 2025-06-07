feacbeb864ca875b87251b43ae4f4febf817168e
Your Name <you@example.com>
2025-06-07 14:50:06 +0530

    0.1

--- src/bar/ingredient.rs
+++ src/bar/ingredient.rs
@@ -188,46 +188,96 @@ pub fn get_ice_gels(
     let blue_icegel = Ingredient {
         name: "Blue Icegel".to_string(),
         description: "Cools down drinks".to_string(),
-        ingredient_profile: blue_icegel_profile,
+        ingredient_profile: blue_icegel_profile.clone(),
     };
     let red_icegel_ingredient = Ingredient {
         name: "Red Icegel".to_string(),
         description: "Cools down drinks".to_string(),
-        ingredient_profile: red_icegel_profile,
+        ingredient_profile: red_icegel_profile.clone(),
     };
     let green_icegel_ingredient = Ingredient {
         name: "Green Icegel".to_string(),
         description: "Cools down drinks".to_string(),
-        ingredient_profile: green_icegel_profile,
+        ingredient_profile: green_icegel_profile.clone(),
     };
+
+    // Profiles for smaller ingredients
+    let small_blue_icegel_profile = IngredientProfile {
+        size: 0.1,
+        taste: IngredientTaste::None,
+        primary_effect: PrimaryEffect::Calming,
+        secondary_effect: SecondaryEffect::Sedated(EffectCondition {
+            volume_needed: 90.0,
+            catalyst: None,
+        }),
+        hazard: None,
+    };
+    let small_red_icegel_profile = IngredientProfile {
+        size: 0.1,
+        taste: IngredientTaste::Spicy,
+        primary_effect: PrimaryEffect::Energizing,
+        secondary_effect: SecondaryEffect::Agitated(EffectCondition {
+            volume_needed: 90.0,
+            catalyst: None,
+        }),
+        hazard: None,
+    };
+    let small_green_icegel_profile = IngredientProfile {
+        size: 0.1,
+        taste: IngredientTaste::Sweet,
+        primary_effect: PrimaryEffect::Healing,
+        secondary_effect: SecondaryEffect::Euphoric(EffectCondition {
+            volume_needed: 90.0,
+            catalyst: None,
+        }),
+        hazard: None,
+    };
+
+    // Smaller ingredients
+    let small_blue_icegel = Ingredient {
+        name: "Small Blue Icegel".to_string(),
+        description: "A small blue icegel".to_string(),
+        ingredient_profile: small_blue_icegel_profile,
+    };
+    let small_red_icegel = Ingredient {
+        name: "Small Red Icegel".to_string(),
+        description: "A small red icegel".to_string(),
+        ingredient_profile: small_red_icegel_profile,
+    };
+    let small_green_icegel = Ingredient {
+        name: "Small Green Icegel".to_string(),
+        description: "A small green icegel".to_string(),
+        ingredient_profile: small_green_icegel_profile,
+    };
+
     vec![
         (
-            blue_icegel.clone(),
+            small_blue_icegel.clone(),
             blue_icegel_sprite.clone(),
             Transform::from_xyz(-400.0, 150.0, 1.0),
         ),
         (
-            red_icegel_ingredient.clone(),
+            small_red_icegel.clone(),
             red_icegel_sprite.clone(),
             Transform::from_xyz(-200.0, 150.0, 1.0),
         ),
         (
-            green_icegel_ingredient.clone(),
+            small_green_icegel.clone(),
             green_icegel_sprite.clone(),
             Transform::from_xyz(0.0, 150.0, 1.0),
         ),
         (
-            blue_icegel.clone(),
+            small_blue_icegel.clone(),
             blue_icegel_sprite.clone(),
             Transform::from_xyz(-400.0, 50.0, 1.0),
         ),
         (
-            red_icegel_ingredient.clone(),
+            small_red_icegel.clone(),
             red_icegel_sprite.clone(),
             Transform::from_xyz(-200.0, 50.0, 1.0),
         ),
         (
-            green_icegel_ingredient.clone(),
+            small_green_icegel.clone(),
             green_icegel_sprite.clone(),
             Transform::from_xyz(0.0, 50.0, 1.0),
         ),
