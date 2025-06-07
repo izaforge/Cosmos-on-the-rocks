b5bdfdc86864813a54d7d4328e2752d1e47934ee
Swapnaneel-afk <swapnaneelphysics@gmail.com>
2025-06-07 19:46:34 +0530

    check

--- src/constants.rs
+++ src/constants.rs
@@ -11,3 +11,4 @@ pub const TEXT_COLOR: Color = Color::srgb(0.10, 0.80, 0.60);
 pub const RED: Color = Color::srgb(1.0, 0.0, 0.0);
 pub const BLUE: Color = Color::srgb(0.0, 1.0, 0.0);
 pub const GREEN: Color = Color::srgb(0.0, 0.0, 1.0);
+//check
